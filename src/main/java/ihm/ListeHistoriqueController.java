205c910c7608ed32578bd96a8d8ac4710c30af74
clementaouali3011 <clementaouali26@gmail.com>
2025-04-28 16:43:28 +0200

    historique

--- src/main/java/ihm/ListeHistoriqueController.java
+++ src/main/java/ihm/ListeHistoriqueController.java
@@ -8,6 +8,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import model.Depot;
+import model.Utilisateur;
 import utils.DatabaseConnection;
 
 import java.sql.Connection;
@@ -37,44 +38,55 @@ public class ListeHistoriqueController {
 
     private void loadHistorique() {
         try {
+            // Crée la connexion à la base de données
             Connection conn = DatabaseConnection.getConnection();
             DepotDAO dao = new DepotDAO(conn);
 
-            List<Depot> depots = dao.getByPoubelleId(poubelleId);
+            // Utilise l'ID de l'utilisateur pour récupérer son historique de dépôts
+            List<Depot> depots = dao.getByUtilisateurId(utilisateurId); // Récupère les dépôts pour l'utilisateur
 
+            // Crée une ObservableList pour afficher les dépôts dans la TableView
             ObservableList<Depot> data = FXCollections.observableArrayList(depots);
 
-            // Formater les colonnes
+            // Formate la colonne "date"
             dateCol.setCellValueFactory(cellData ->
                     new javafx.beans.property.SimpleStringProperty(
                             cellData.getValue().getHeureDepot().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"))
                     )
             );
 
+            // Formate la colonne "type" (type de déchet)
             typeCol.setCellValueFactory(cellData ->
                     new javafx.beans.property.SimpleStringProperty(cellData.getValue().getType().name())
             );
 
+            // Formate la colonne "points" (points attribués)
             pointsCol.setCellValueFactory(cellData ->
                     new javafx.beans.property.SimpleIntegerProperty(cellData.getValue().getPoints()).asObject()
             );
 
+            // Formate la colonne "utilisateur" (nom de l'utilisateur)
             utilisateurCol.setCellValueFactory(cellData -> {
-                if (cellData.getValue().getUtilisateur() != null && cellData.getValue().getUtilisateur().getId() != 0) {
-                    return new javafx.beans.property.SimpleStringProperty(
-                            String.valueOf(cellData.getValue().getUtilisateur().getId())
-                    );
+                Utilisateur utilisateur = cellData.getValue().getUtilisateur();
+                if (utilisateur != null) {
+                    return new javafx.beans.property.SimpleStringProperty(utilisateur.getNom()); // Affiche le nom de l'utilisateur
                 } else {
                     return new javafx.beans.property.SimpleStringProperty("Inconnu");
                 }
             });
 
-
-
+            // Ajoute les données à la TableView
             historiqueTable.setItems(data);
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
+    private int utilisateurId;
+
+    public void setUtilisateurId(int utilisateurId) {
+        this.utilisateurId = utilisateurId;
+        loadHistorique();  // Charge l'historique dès qu'on a l'ID de l'utilisateur
+    }
+
 }
